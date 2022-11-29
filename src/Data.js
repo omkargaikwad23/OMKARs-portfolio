883334f7358df74d0b2f5991abdebc632896a804
omkargaikwad23 <omkargaikwad9552@gmail.com>
2022-11-29 17:34:49 +0530

    fixing

--- src/Data.js
+++ src/Data.js
@@ -35,7 +35,7 @@ export const CodingHandles = [
     id: 4,
     site: "Hackerrank",
     handleName: "omg_pict_23",
-    desc: "5 stars archived in problem solving",
+    desc: "Achieved 5 stars in problem solving",
     url: "https://www.hackerrank.com/omg_pict_23",
     icon: SiHackerrank,
   },
@@ -55,7 +55,7 @@ export const EducationData = [
     role: "BE in Computer Engineering",
     duration: "September, 2021 - March, 2021",
     org: "SCTR's Pune Institute of Computer Technology",
-    tasks: ["Pursuing bachelor's degree in computer engineering"],
+    tasks: ["Pursuing bachelor's degree in computer engineering."],
   },
   {
     id: 2,
@@ -63,8 +63,8 @@ export const EducationData = [
     duration: "July, 2017 - March, 2019",
     org: "Rajarshi Shahu Junior Science College, Latur",
     tasks: [
-      "Stood first in a batch of 130 students in MHT-CET exam with 99.31 percentile",
-      "Secured 77.23% in the HSC board exam",
+      "Stood first in a batch of 130 students in MHT-CET exam with 99.31 percentile.",
+      "Secured 77.23% in the HSC board exam.",
     ],
   },
   {
@@ -73,7 +73,7 @@ export const EducationData = [
     duration: "July, 2014 - March, 2017",
     org: "Shri Sant Goroba Kaka Vidyalaya",
     tasks: [
-      "Stood first in the school and exam center(among 7 schools) with 96.40% in SSC board exam",
+      "Stood first in the school and exam center(among 7 schools) with 96.40% in SSC board exam.",
     ],
   },
 ];
@@ -85,8 +85,8 @@ export const InternshipsData = [
     duration: "09/2021 - 12/2021",
     org: "Tech Mahindra",
     tasks: [
-      "Worked on design and development of a full-stack web application",
-      "Collaborated to suggest a suitable product architecture for the website",
+      "Worked on design and development of a full-stack web application.",
+      "Collaborated to suggest a suitable product architecture for the website.",
     ],
   },
   {
@@ -95,9 +95,9 @@ export const InternshipsData = [
     duration: "05/2021 - 07/2021",
     org: "Solapur Rural Police Transfer",
     tasks: [
-      "Worked on the project for a real-time problem for general transfer of Solapur Police Staff",
-      "Developed and implemented web applications using SCRUM Agile methods",
-      "Conducted testing & validation to ensure the software is working properly",
+      "Worked on the project for a real-time problem for general transfer of Solapur Police Staff.",
+      "Developed and implemented web applications using SCRUM Agile methods.",
+      "Conducted testing & validation to ensure the software is working properly.",
     ],
   },
   {
@@ -106,7 +106,7 @@ export const InternshipsData = [
     duration: "04/2022 - Present",
     org: "Interview Mocha",
     tasks: [
-      "Creating new coding questions related to Data Structures and Algorithms, generating test cases forthe problems and writing effective solutions",
+      "Creating new coding questions related to Data Structures and Algorithms, generating test cases forthe problems and writing effective solutions.",
     ]
   }
 ];
@@ -118,7 +118,7 @@ export const VolunteersData = [
     duration: "September, 2020 - Present",
     org: "PICT ACM Student Chapter",
     tasks: [
-      "Conducted introductory sessions on web development domain for first-year students",
+      "Conducted online technical SIGs on Web Development impacting over 200 students under PICT ACM chapter.",
     ],
   },
   {
@@ -127,7 +127,7 @@ export const VolunteersData = [
     duration: "October, 2021 - Present",
     org: "PICT Entrepreneurship Development Cell ",
     tasks: [
-      "Involved in the marketing for The Original Pitch - The flagship event conducted by entrepreneurship cell",
+      "Involved in the marketing of The Original Pitch - The flagship event conducted by entrepreneurship cell.",
     ],
   },
 ];
--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -10,7 +10,7 @@ const projects = [
   {
     id: uuidv4(),
     name: "Solapur Police Transfer Website",
-    desc: "A full stack Web application for general transfer of Solapur Police Staff. This website helps to transfer records of 400-500 policemen, completing the process 80% faster than manual human effort",
+    desc: "A full stack Web application for general transfer of Solapur Police Staff. This website helps to transfer records of 400-500 policemen, completing the process 80% faster than manual human effort.",
     img: Police,
     tech: ["ReactJS", "Node.js", "Express.js", "Firebase"],
     url: "https://spstaffdata1.web.app",
@@ -18,7 +18,7 @@ const projects = [
   {
     id: uuidv4(),
     name: "StartStock",
-    desc: "A website which helps to learn the stock market virtually by reading articles, different case studies, playing quizzes, etc. User can read FAQs orregisterto ask their doubts",
+    desc: "A website which helps to learn the stock market virtually by reading articles, different case studies, playing quizzes, etc. User can read FAQs orregisterto ask their doubts.",
     img: Startstock,
     tech: ["HTML", "CSS", "ReactJS", "Bootstrap", "MongoDB"],
     url: "https://startstock.netlify.app"
@@ -26,7 +26,7 @@ const projects = [
   { 
     id: uuidv4(),
     name: "Virtual Farmers Market",
-    desc: "Created a website that connects farmers to sell theirfruits and vegetables, to the retailers. It helps both the interested parties to make a deal and directly communicate with each other",
+    desc: "Created a website that connects farmers to sell theirfruits and vegetables, to the retailers. It helps both the interested parties to make a deal and directly communicate with each other.",
     img: Vfm,
     tech: ["ReactJS", "Node.js", "MongoDB"],
     url: "https://krishimitra.netlify.app"
@@ -34,7 +34,7 @@ const projects = [
   {
     id: uuidv4(),
     name: "Flipkart-clone 2.0",
-    desc: "Developed a website Flipkart-clone using React library. It has a voice assistant feature to navigate and filter the products by voice commands",
+    desc: "Developed a website Flipkart-clone using React library. It has a voice assistant feature to navigate and filter the products by voice commands.",
     img: flipkart,
     tech: ["ReactJS", "Material UI"],
     url: "https://flipkart-clone-20.web.app/",
@@ -42,7 +42,7 @@ const projects = [
   {
     id: uuidv4(),
     name: "Movie Search App",
-    desc: "This is a movie search app build using OMDB API",
+    desc: "This is a movie search app build using OMDB API.",
     img: Movie,
     tech: ["ReactJS", "OMDB api"],
     url: "https://movie-search-app-by-omg.web.app/",
@@ -50,7 +50,7 @@ const projects = [
   {
     id: uuidv4(),
     name: "Iron Man Mask",
-    desc: "Created helmet design using Python GUI - tkinter package",
+    desc: "Created helmet design using Python GUI - tkinter package.",
     img: IronMan,
     tech: ["Python GUI - Tkinter"],
     url: "https://github.com/omkargaikwad23/Iron-Man-Helmet-Design-Tkinter"
